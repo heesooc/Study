83509f2b2d081c5cee70a72ce737248d595fdd6d
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-01-29 11:16:19 +0900

    [조희수] 프로퍼티 적용

--- Assets/02.Scripts/Capsule/PeopleTest.cs
+++ Assets/02.Scripts/Capsule/PeopleTest.cs
@@ -8,10 +8,15 @@ public class PeopleTest : MonoBehaviour
    
     void Start()
     {
-        People people = new People("조희수", 25);
+        People people = new People("조희수", 25, 169f, "INFP");
         people.SetAge(26);
         people.SetAge(-3);
 
+        people.Height = -10;        // 프로퍼티 set 접근자 사용
+        Debug.Log(people.Height);   // 프로퍼티 get 접근자 사용
+
+        people.MBTI = "AAA"; // 실습. MBTI에 대해서 프로퍼티를 적용해보세요. 
+        Debug.Log(people.MBTI); 
         //people.Age = -3; // 무결성이 깨졌다!
         // 무결성이란 데이터의 정확성, 일관성, 유효성
 
